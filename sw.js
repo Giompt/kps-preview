27d79bb89c417d31dbad1a7516fb62a2c79678be
Giompt <marsellagiovanni5885@gmail.com>
2026-07-12 22:42:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE='kps-v15';const FILES=['./','./index.html','./manifest.webmanifest','./program-data.js','./exercise-library.js','./app.js','./anatomy-avatar.png','./anatomy-chest.png','./anatomy-shoulders.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='kps-v16';const FILES=['./','./index.html','./manifest.webmanifest','./program-data.js','./exercise-library.js','./app.js','./anatomy-avatar.png','./anatomy-chest.png','./anatomy-shoulders.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
